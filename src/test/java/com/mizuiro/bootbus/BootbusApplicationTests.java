099bbf6ab83347ba0ce8a635d00df4aa05cd6784
MizuiroL <mizuirolusken@gmail.com>
2020-07-26 13:05:30 +0200

    Initial commit

---
+++ src/test/java/com/mizuiro/bootbus/BootbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mizuiro.bootbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
