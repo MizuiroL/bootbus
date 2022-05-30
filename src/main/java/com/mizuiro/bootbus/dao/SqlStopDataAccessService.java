389481e3ed28ecbc3443ed5407ecb2be73e16a7d
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2022-05-30 16:12:09 +0200

    Fixed indenting

--- src/main/java/com/mizuiro/bootbus/dao/SqlStopDataAccessService.java
+++ src/main/java/com/mizuiro/bootbus/dao/SqlStopDataAccessService.java
@@ -39,7 +39,7 @@ public class SqlStopDataAccessService implements StopDao {
 
     private RowMapper<Stop> mapStop() {
         return (resultSet, i) -> {
-          String stopId = resultSet.getString("stop_id");
+            String stopId = resultSet.getString("stop_id");
             String stopName = resultSet.getString("stop_name");
             double stopLat = resultSet.getDouble("stop_lat");
             double stopLon = resultSet.getDouble("stop_lon");
